d6310cf9c8c4a6f90fac99f1d97303b216d1cc27
Frederik PS <frederik.p.stender@gmail.com>
2022-05-22 20:21:41 +0200

    func

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-const express = require('express')
+const express = require("express");
 const Arena = require("are.na");
-const app = express()
-const port = process.env.PORT || 3000
-const arena = new Arena();
+const app = express();
+const port = process.env.PORT || 3000;
 
 // "place-hyufowbjwka"
 
-app.get('/', (req, res) => {
-  res.redirect('/place-hyufowbjwka/1');
-})
+app.get("/", function (req, res) {
+  res.redirect("/place-hyufowbjwka/1");
+});
 
-app.get('/:channel', (req, res) => {
+app.get("/:channel", function (req, res) {
   res.redirect(`/${req.params.channel}/1`);
-})
+});
 
-app.get('/:channel/:page', (req, res) => {
+app.get("/:channel/:page", function (req, res) {
+  const arena = new Arena();
   let contents = [];
   arena
     .channel(req.params.channel)
     .get({
+      direction: "desc",
       page: req.params.page, per: 34,
-      direction: 'desc',
-      sort: 'position'
+      sort: "position"
     })
     .then(function (channel) {
       channel.contents.forEach(function (item) {
         if (item.image) {
           contents.push(`<img src="${item.image.display.url}">`);
         }
-      })
+      });
       contents.unshift(`<main><a href="/${req.params.channel}/${parseInt(req.params.page) + 1}">`);
-      contents.push(`</a></main>`)
+      contents.push(`</a></main>`);
       contents = contents.join("");
-      res.send(contents)
+      res.send(contents);
     })
     .catch(function (error) {
-      res.send(error)
-    })
-  })
+      res.send(error);
+    });
+});
 
 
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}`);
+});
