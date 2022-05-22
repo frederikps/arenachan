7cd4a0ff9b7a79e4bedfff844a75c64177cc6053
Frederik PS <frederik.p.stender@gmail.com>
2022-05-22 16:45:39 +0200

    exp

--- index.js
+++ index.js
@@ -16,23 +16,30 @@ app.get('/:channel', (req, res) => {
 
 app.get('/:channel/:page', (req, res) => {
   let contents = [];
-    arena
-  .channel(req.params.channel)
-  .get({ page: req.params.page, per: 34,
-    direction: 'desc',
-    sort: 'position'
-   })
-  .then(function(channel){
-    channel.contents.forEach(function(item) {
-      if (item.image) {
-        contents.push(`<img src="${item.image.display.url}">`);
-       }
+  arena
+    .channel(req.params.channel)
+    .get({
+      page: req.params.page, per: 34,
+      direction: 'desc',
+      sort: 'position'
     })
-    res.send(`<main><a href="/${req.params.channel}/${parseInt(req.params.page)+1}">`+contents.join("")+`</a></main>`)
-    });
+    .then(function (channel) {
+      channel.contents.forEach(function (item) {
+        if (item.image) {
+          contents.push(`<img src="${item.image.display.url}">`);
+        }
+      })
+      contents.unshift(`<main><a href="/${req.params.channel}/${parseInt(req.params.page) + 1}">`);
+      contents.push(`</a></main>`)
+      contents = contents.join("");
+      res.send(contents)
+    })
+    .catch(function (error) {
+      res.send(error)
+    })
   })
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
