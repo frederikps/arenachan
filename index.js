3bd4eb3d9120c7595e42cf49c543dc3ac3539536
Frederik PS <frederik.p.stender@gmail.com>
2022-05-23 12:34:56 +0200

    search

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const Arena = require("are.na");
-const eta = require("eta")
+const eta = require("eta");
 const app = express();
 const port = process.env.PORT || 3000;
-app.engine("eta", eta.renderFile)
+app.engine("eta", eta.renderFile);
 
-app.set("view engine", "eta")
+app.set("view engine", "eta");
 
-app.set("views", "./views")
+app.set("views", "./views");
 
 // "place-hyufowbjwka"
 
 app.get('/', (req, res) => {
-  if (req.query.channel) { 
-    
-    res.redirect(`/${req.query.channel}/1`);
-   } else
-  {
+  if (req.query.channel) {
 
-    res.redirect('/place-hyufowbjwka/1');
+    res.redirect(`/${req.query.channel}/1`);
+  } else {
+
+    res.send('');
   }
 });
 
@@ -50,6 +49,28 @@ app.get("/:channel/:page", function (req, res) {
         next: `/${req.params.channel}/${parseInt(req.params.page) + 1}`
       });
     }).catch(function () {
+      arena
+        .search(req.params.channel)
+        .blocks({
+
+          page: req.params.page, per: 34,
+
+        })
+        .then(function (channel) {
+          channel.forEach(function (item) {
+            if (item.image) {
+              contents.push(`<img src="${item.image.display.url}">`);
+            }
+          });
+          contents = contents.join("");
+          res.render("template", {
+            body: contents,
+            previous: `/${req.params.channel}/${parseInt(req.params.page) - 1}`,
+            next: `/${req.params.channel}/${parseInt(req.params.page) + 1}`
+          });
+        });
+    })
+    .catch(function () {
       res.send("");
     });
 });
